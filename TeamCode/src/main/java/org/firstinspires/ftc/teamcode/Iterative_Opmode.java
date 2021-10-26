f36e10c263c77d9908f4b7e0af7c0df80ee8b237
c23fk <c23fk@dalton.org>
2021-10-25 21:03:00 -0400

    Working setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iterative_Opmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Iterative_Opmode.java
@@ -67,7 +67,7 @@ public class Iterative_Opmode extends OpMode
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
-
+        
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
